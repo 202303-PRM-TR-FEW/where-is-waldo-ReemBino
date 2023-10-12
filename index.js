47bfc0445ec9dd31a5c00d3469363eea500d2d23
Reem bino <reembinoo@gmail.com>
2023-10-12 18:49:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const example = [
-  ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
+["A", "A", "A"],
+["A", "A", "A"],
+["A", "B", "A"]
+];
 
 const whereIsWaldo = (matrix) => {
-  
+for (let i = 0; i < matrix.length; i++) {
+for (let j = 0; j < matrix[i].length; j++) {
+if (matrix[i][j] === "B") {
+return [i, j];
 }
+}
+}
+}
+
+console.log(whereIsWaldo(example));
